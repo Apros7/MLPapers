2919a480dd47086ff5cf44cf275e126192e3fef2
apros7 <lucas.vilsen@gmail.com>
2024-03-28 15:28:36 +0100

    first try autoencoder

--- Autoencoder/autoencoders.py
+++ Autoencoder/autoencoders.py
@@ -1,11 +1,34 @@
+from tinygrad import Tensor
+import tinygrad.nn as nn
+
 class Base:
-    def _method1():
-        return None
+    def __init__(self, input, hidden, output):
+        self.layers = [
+            nn.Linear(input, hidden),
+            nn.Linear(hidden, output)
+        ]
+    def __call__(self, x):
+        for i, layer in enumerate(self.layers):
+            x = layer(x).tanh() if i != len(self.layers) - 1 else layer(x)
+        return x
 
 class Encoder(Base):
     def __init__(self) -> None:
         super().__init__()
 
+    def __call__(self):
+        pass
+
 class Decoder(Base):
     def __init__(self) -> None:
         super().__init__()
+    
+    def __call__(self):
+        pass
+
+class AutoEncoder:
+    def __init__(self, original_size, hidden_size, compressing_size):
+        self.encoder = Base(original_size, hidden_size, compressing_size)
+        self.decoder = Base(compressing_size, hidden_size, original_size)
+    def __call__(self, x):
+        return self.decoder(self.encoder(x))
