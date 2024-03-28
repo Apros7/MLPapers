57187c9b5fa7703f422603a2535911421a4a77d5
apros7 <lucas.vilsen@gmail.com>
2024-03-28 13:50:01 +0100

    base model autoencoders

---
+++ Autoencoder/autoencoders.py
@@ -0,0 +1,11 @@
+class Base:
+    def _method1():
+        return None
+
+class Encoder(Base):
+    def __init__(self) -> None:
+        super().__init__()
+
+class Decoder(Base):
+    def __init__(self) -> None:
+        super().__init__()
