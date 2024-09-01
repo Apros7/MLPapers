fe52df5e98125172c15a7ed70531dd3ea5d14350
apros7 <lucas.vilsen@gmail.com>
2024-09-01 19:44:08 +0200

    all constants

---
+++ AttentionIsAllYouNeed/train.py
@@ -0,0 +1,1 @@
+# training of simple transformer on ___ dataset
--- AttentionIsAllYouNeed/transformer.py
+++ AttentionIsAllYouNeed/transformer.py
@@ -1,3 +1,19 @@
+import torch
+
+N_encoder_layers = 6
+N_decoder_layers = 6
+N_encoder_heads = 8
+N_decoder_heads = 8
+d_model = 512
+d_ff = 2048
+
+# multi-head attention
+h = 8
+d_k = d_model/h
+d_v = d_model/h
+
+def attention(q, k, v): # dot scaling + 1/d_model**0.5 scaling
+    return torch.softmax(q @ k.T / (d_model ** 0.5), dim=-1) @ v
 
 class Transformer:
     def __init__(self) -> None:
